95014d6efca6f3f77f5bf4e30834e11f8b3d57f1
mick89.mac@gmail.com <mick89.mac@gmail.com@60b3391c-5998-32ed-16c4-3778d7d19ad7>
2012-12-11 18:01:07 +0000

    updated copyright information in source and credits

--- Armot/src/console/TChooseInterface.java
+++ Armot/src/console/TChooseInterface.java
@@ -1,3 +1,22 @@
+/*
+    ARMOT  Arp Monitoring Tool
+    
+    Copyright (C) 2012  Massaro Michele, Tomasello Alex
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package console;
 
 import java.util.Scanner;
@@ -8,6 +27,16 @@ import jpcap.NetworkInterfaceAddress;
 public class TChooseInterface {
 
 	public TChooseInterface() {
+
+		System.out
+				.println("Armot Copyright (C) 2012  Massaro Michele, Tomasello Alex");
+		System.out.println("This program comes with ABSOLUTELY NO WARRANTY;");
+		System.out
+				.println("This is free software, and you are welcome to redistribute it");
+		System.out
+				.println("under the terms of the GNU General Public License as published by");
+		System.out.println("the Free Software Foundation\n");
+
 		jpcap.NetworkInterface[] devices = JpcapCaptor.getDeviceList();
 
 		if (devices.length == 0) {
